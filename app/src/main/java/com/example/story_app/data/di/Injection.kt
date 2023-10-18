98fa6b3df39f13a05ae4c55151a850510a87cccd
azkifairuz <azkiajmal@gmail.com>
2023-10-19 00:57:18 +0700

    feat: injection

---
+++ app/src/main/java/com/example/story_app/data/di/Injection.kt
@@ -0,0 +1,15 @@
+package com.example.story_app.data.di
+
+import android.content.Context
+import com.example.story_app.data.ApiConfig
+import com.example.story_app.data.local.AuthPreference
+import com.example.story_app.data.repo.StoryRepository
+
+object Injection {
+    fun provideRepository(context: Context): StoryRepository {
+    val pref = AuthPreference(context)
+    val user = pref.getUser()
+    val apiService = ApiConfig.getApiService(user.token)
+    return StoryRepository.getInstance(apiService, pref)
+}
+}
