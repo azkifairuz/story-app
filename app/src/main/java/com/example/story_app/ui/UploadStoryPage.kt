df8065c677d8e74437432191fd57a2193c96f0e3
azkifairuz <azkiajmal@gmail.com>
2023-10-22 23:35:50 +0700

    feat:detail story page layout

---
+++ app/src/main/java/com/example/story_app/ui/UploadStoryPage.kt
@@ -0,0 +1,20 @@
+package com.example.story_app.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.story_app.R
+
+class UploadStoryPage : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_upload_story_page, container, false)
+    }
+
+}
