3c4862d8037839c8b0b97201442096b5e201e233
azkifairuz <azkiajmal@gmail.com>
2023-10-22 20:40:55 +0700

    feat: detail story page

---
+++ app/src/main/java/com/example/story_app/ui/DetailStoryPage.kt
@@ -0,0 +1,17 @@
+package com.example.story_app.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.story_app.R
+class DetailStoryPage : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail_story_page, container, false)
+    }
+}
