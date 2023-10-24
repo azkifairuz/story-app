0627fd2d042d225035a524e19e106a61b6c366d7
azkifairuz <azkiajmal@gmail.com>
2023-10-24 15:13:42 +0700

    query post

--- app/src/main/java/com/example/story_app/data/ApiService.kt
+++ app/src/main/java/com/example/story_app/data/ApiService.kt
@@ -4,12 +4,16 @@ import com.example.story_app.data.response.DetailStoryResponse
 import com.example.story_app.data.response.ErrorResponse
 import com.example.story_app.data.response.LoginResponse
 import com.example.story_app.data.response.StoryResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
 import retrofit2.http.Path
 
 interface ApiService {
@@ -33,4 +37,14 @@ interface ApiService {
     fun detailStory(
         @Path("id") id: String
     ): Call<DetailStoryResponse>
+
+    @POST("stories")
+    @Multipart()
+    suspend fun postStory(
+        @Header("Authorization") token: String,
+        @Part file: MultipartBody.Part,
+        @Part("description") description: RequestBody,
+        @Part("lat") lat: RequestBody?,
+        @Part("lon") lon: RequestBody?
+    ): ErrorResponse
 }
--- app/src/main/java/com/example/story_app/ui/CameraActivity.kt
+++ app/src/main/java/com/example/story_app/ui/CameraActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.OrientationEventListener
+import android.view.Surface
 import android.widget.Toast
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
@@ -96,6 +98,30 @@ class CameraActivity : AppCompatActivity() {
         )
     }
 
+    private val orientationEventListener by lazy {
+        object : OrientationEventListener(this) {
+            override fun onOrientationChanged(orientation: Int) {
+                if (orientation == ORIENTATION_UNKNOWN) {
+                    return
+                }
+                val rotation = when (orientation) {
+                    in 45 until 135 -> Surface.ROTATION_270
+                    in 135 until 225 -> Surface.ROTATION_180
+                    in 225 until 315 -> Surface.ROTATION_90
+                    else -> Surface.ROTATION_0
+                }
+                imageCapture?.targetRotation = rotation
+            }
+        }
+    }
+    override fun onStart() {
+        super.onStart()
+        orientationEventListener.enable()
+    }
+    override fun onStop() {
+        super.onStop()
+        orientationEventListener.disable()
+    }
 
     companion object {
         private const val TAG = "CameraActivity"
