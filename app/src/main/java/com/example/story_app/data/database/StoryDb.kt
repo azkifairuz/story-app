3aebb7d7bcde6290882f0fc7e2d1ef25148493a2
azkifairuz <azkiajmal@gmail.com>
2023-11-05 01:53:12 +0700

    update story db

--- app/src/main/java/com/example/story_app/data/database/StoryDb.kt
+++ app/src/main/java/com/example/story_app/data/database/StoryDb.kt
@@ -1,6 +1,8 @@
 package com.example.story_app.data.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.story_app.data.response.ListStoryItem
 import com.example.story_app.data.response.ListStoryItemWithMap
@@ -14,4 +16,22 @@ import com.example.story_app.data.response.ListStoryItemWithMap
 abstract class StoryDb: RoomDatabase() {
     abstract fun storyDao(): StoryDao
     abstract fun remoteKeysDao(): RemoteKeysDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: StoryDb? = null
+
+        @JvmStatic
+        fun getDatabase(context: Context): StoryDb {
+            return INSTANCE ?: synchronized(this) {
+                INSTANCE ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    StoryDb::class.java, "story_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+                    .also { INSTANCE = it }
+            }
+        }
+    }
 }
